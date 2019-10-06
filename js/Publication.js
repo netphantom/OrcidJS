f8b6c08081dad999cd96a65ff0b669e0e3fc73b7
Fabio Cassano <net_phantom@gmx.com>
2019-10-06 16:59:40 +0100

    Work in progress
    More classes, bug fix and code optimization

--- js/Publication.js
+++ js/Publication.js
@@ -1,7 +1,8 @@
 class Publication{
-    constructor(title, year) {
+    constructor(title, year, doi) {
         this._title = title;
         this._year = year;
+        this._doi = doi;
     }
 
     get title() {
@@ -11,14 +12,21 @@ class Publication{
     get year() {
         return this._year;
     }
+
+    get doi() {
+        return this._doi;
+    }
+
     printDetails(){
 
     }
 }
 
+// The following classes implement the different types of publication based on the latex syntax
+// This class implements a general Article references
 class Article extends Publication {
-    constructor(author, title, journal, year, volume, number, pages, month, note) {
-        super(title, year);
+    constructor(author, title, doi, journal, year, volume, number, pages, month, note) {
+        super(title, year, doi);
         this._author = author;
         this._journal = journal;
         this._volume = volume;
@@ -72,11 +80,11 @@ class Article extends Publication {
     }
 }
 
+// This class implements a book reference
 class Book extends Publication {
-    constructor(authorOrEditor, title, publisher, year,volumeOrNumber, series, address, edition, month, note) {
-        super(title, year);
+    constructor(authorOrEditor, title, doi, publisher, year,volumeOrNumber, series, address, edition, month, note) {
+        super(title, year, doi);
         this._authorOrEditor = authorOrEditor;
-        this._title = title;
         this._publisher = publisher;
         this._volumeOrNumber = volumeOrNumber;
         this._series = series;
@@ -90,9 +98,6 @@ class Book extends Publication {
         return this._authorOrEditor;
     }
 
-    get title() {
-        return this._title;
-    }
 
     get publisher() {
         return this._publisher;
@@ -121,6 +126,7 @@ class Book extends Publication {
     get note() {
         return this._note;
     }
+
     printDetails() {
         super.printDetails();
         let output = "";
@@ -138,9 +144,10 @@ class Book extends Publication {
     }
 }
 
+// This class implements a booklet reference
 class Booklet extends Publication {
-    constructor(title,author, howpublished, address, month, year, note) {
-        super(title, year);
+    constructor(title,author, doi, howpublished, address, month, year, note) {
+        super(title, year, doi);
         this._author = author;
         this._howpublished = howpublished;
         this._address = address;
@@ -170,12 +177,22 @@ class Booklet extends Publication {
 
     printDetails() {
         super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.howpublished + '<br>';
+        output += this.address + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
     }
 }
 
+// This class implements a inbook reference
 class Inbook extends Publication {
-    constructor(authorOrEditor, title, chapterAndOrPages, publisher, year, volumeOrNumber, series, type, address, edition, month, note) {
-        super(title, year);
+    constructor(authorOrEditor, title, doi, chapterAndOrPages, publisher, year, volumeOrNumber, series, type, address, edition, month, note) {
+        super(title, year, doi);
         this._authorOrEditor = authorOrEditor;
         this._chapterAndOrPages = chapterAndOrPages;
         this._volumeOrNumber = volumeOrNumber;
@@ -229,12 +246,27 @@ class Inbook extends Publication {
     }
     printDetails() {
         super.printDetails();
+        let output = "";
+        output += '<div>'+ this.authorOrEditor + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.chapterAndOrPages + '<br>';
+        output += this.publisher + '<br>';
+        output += this.volumeOrNumber + '<br>';
+        output += this.series + '<br>';
+        output += this.type + '<br>';
+        output += this.address + '<br>';
+        output += this.edition + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
     }
 }
 
+// This class implements a incollection reference
 class Incollection extends Publication {
-    constructor( author, title, booktitle, publisher, year,  editor, volumeOrNumber, series, type, chapter, pages, address, edition, month, note) {
-        super(title, year);
+    constructor( author, title, doi, booktitle, publisher, year,  editor, volumeOrNumber, series, type, chapter, pages, address, edition, month, note) {
+        super(title, year, doi);
         this._author = author;
         this._bookTitle = booktitle;
         this._publisher = publisher;
@@ -303,33 +335,49 @@ class Incollection extends Publication {
     }
     printDetails() {
         super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.bookTitle + '<br>';
+        output += this.publisher + '<br>';
+        output += this.editor + '<br>';
+        output += this.volumeOrNumber + '<br>';
+        output += this.chapter + '<br>';
+        output += this.pages + '<br>';
+        output += this.series + '<br>';
+        output += this.type + '<br>';
+        output += this.address + '<br>';
+        output += this.edition + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
     }
 }
 
+// This class implements a inproceedings reference
 class Inproceedings extends Publication {
-    constructor(author, title, booktitle, year, editor, volumeOrNumber, series, pages, address, month, organization, publisher, note) {
-        super(title, year);
-        this._booktitile = booktitle;
+    constructor(author, title, doi, bookTitle, year, editor, volumeOrNumber, series, pages, address, month, organization, publisher, note) {
+        super(title, year, doi);
+        this._bookTitle = bookTitle;
         this._author = author;
         this._editor = editor;
         this._volumeOrNumber = volumeOrNumber;
         this._series = series;
         this._pages = pages;
-        this._addess = address;
+        this._address = address;
         this._month = month;
         this._organization = organization;
         this._publisher = publisher;
         this._note = note;
-        this._booktitle = booktitle;
-        this._address = address;
     }
 
     get author() {
         return this._author;
     }
 
-    get booktitle() {
-        return this._booktitle;
+    get bookTitle() {
+        return this._bookTitle;
     }
 
     get editor() {
@@ -367,14 +415,31 @@ class Inproceedings extends Publication {
     get note() {
         return this._note;
     }
+
     printDetails() {
         super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.bookTitle + '<br>';
+        output += this.publisher + '<br>';
+        output += this.editor + '<br>';
+        output += this.volumeOrNumber + '<br>';
+        output += this.organization + '<br>';
+        output += this.pages + '<br>';
+        output += this.series + '<br>';
+        output += this.address + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
     }
 }
 
+// This class implements a manual reference
 class Manual extends Publication {
-    constructor(title, author, organization, address, edition, month, year, note) {
-        super(title, year);
+    constructor(title, author, doi, organization, address, edition, month, year, note) {
+        super(title, year, doi);
         this._title = title;
         this._author = author;
         this._organization = organization;
@@ -384,11 +449,52 @@ class Manual extends Publication {
         this._note = note;
     }
 
+    get title() {
+        return this._title;
+    }
+    get author() {
+        return this._author;
+    }
+
+    get organization() {
+        return this._organization;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    get edition() {
+        return this._edition;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    printDetails() {
+        super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.organization + '<br>';
+        output += this.address + '<br>';
+        output += this.edition + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
+    }
 }
 
+// This class implements a general thesis (Phd, master etc.) reference
 class Thesis extends Publication {
-    constructor(author, title, school, year, type, address, month, note) {
-        super(title, year);
+    constructor(author, title, doi, school, year, type, address, month, note) {
+        super(title, year, doi);
         this._author = author;
         this._school = school;
         this._type = type;
@@ -396,11 +502,50 @@ class Thesis extends Publication {
         this._month = month;
         this._note = note;
     }
+
+    get author() {
+        return this._author;
+    }
+
+    get school() {
+        return this._school;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    printDetails() {
+        super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.type + '<br>';
+        output += this.address + '<br>';
+        output += this.school + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
+    }
 }
 
+// This class implements a conference proceedings reference
 class Proceedings extends Publication {
-    constructor(title, year, editor, volumeOrNumber, series, address, publisher, note, month, organization){
-        super(title, year);
+    constructor(title, year, doi, editor, volumeOrNumber, series, address, publisher, note, month, organization){
+        super(title, year, doi);
         this._editor = editor;
         this._volumeOrNumber = volumeOrNumber;
         this._series = series;
@@ -410,11 +555,60 @@ class Proceedings extends Publication {
         this._month = month;
         this._organization = organization;
     }
+
+    get editor() {
+        return this._editor;
+    }
+
+    get volumeOrNumber() {
+        return this._volumeOrNumber;
+    }
+
+    get series() {
+        return this._series;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    get publisher() {
+        return this._publisher;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    get organization() {
+        return this._organization;
+    }
+
+    printDetails() {
+        super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.publisher + '<br>';
+        output += this.editor + '<br>';
+        output += this.volumeOrNumber + '<br>';
+        output += this.series + '<br>';
+        output += this.address + '<br>';
+        output += this.month + '<br>';
+        output += this.organization + '<br><hr>';
+        return output;
+    }
 }
 
+// This class implements implements a tech report reference
 class TechReport extends Publication {
-    constructor(author, title, institution, year,type, number, address, month, note) {
-        super(title, year);
+    constructor(author, title, doi, institution, year,type, number, address, month, note) {
+        super(title, year, doi);
         this._author = author;
         this._institution = institution;
         this._type = type;
@@ -424,13 +618,79 @@ class TechReport extends Publication {
         this._note = note;
     }
 
+    get author() {
+        return this._author;
+    }
+
+    get institution() {
+        return this._institution;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get number() {
+        return this._number;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    printDetails() {
+        super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.institution + '<br>';
+        output += this.type + '<br>';
+        output += this.number + '<br>';
+        output += this.address + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
+    }
 }
 
+// This class implements a unpublished work reference
 class Unpublished extends Publication {
-    constructor(author, title, note, month, year) {
-        super(title, year);
+    constructor(author, title, doi, note, month, year) {
+        super(title, year, doi);
         this._author = author;
         this._note = note;
         this._month = month;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    printDetails() {
+        super.printDetails();
+        let output = "";
+        output += '<div>'+ this.author + '<br>';
+        output += '<b>' + this.title + '</b><br>';
+        output += this.year + '<br>';
+        output += this.month + '<br>';
+        output += this.note + '<br><hr>';
+        return output;
     }
 }
--- js/getOrcidPubs.js
+++ js/getOrcidPubs.js
@@ -7,7 +7,7 @@ function printPubList(orcid, idelement) {
             let linkpub = "https://pub.orcid.org/v2.0/" + orcid + "/work/" + data.group[i]["work-summary"][0]["put-code"];
             httpGet(linkpub).then(function (pubdetails) {
                 pubdetails = JSON.parse(pubdetails);
-                let pub = createPubObj(pubdetails["citation"]["citation-value"]);
+                let pub = new BibtexParser(pubdetails["citation"]["citation-value"]);
                 printElement(pub, idelement);
                 list.push(pub);
             });
@@ -32,49 +32,6 @@ function httpGet(url) {
         request.setRequestHeader("Accept", "application/orcid+json");
         request.send();
     });
-}
-
-function createPubObj(details) {
-
-    let reg = /title *= *\{(.*?)\}/;
-    let s = details.match(reg);
-    let title = s[1];
-
-    reg = /author *= *\{(.*?)\}/;
-    s = details.match(reg);
-    let author = s[1];
-
-    reg = /booktitle *= *\{(.*?)\}/;
-    s = details.match(reg);
-    if (s == null) {
-        reg = /journal *= *\{(.*?)\}/;
-        s = details.match(reg);
-    }
-    let confjournal = s[1];
-
-    reg = /volume *= *\{(.*?)\}/;
-    s = details.match(reg);
-    let volume = null;
-    if (s != null) {
-        volume = s[1];
-    }
-
-    reg = /pages *= *\{(.*?)\}/;
-    s = details.match(reg);
-    let pages = s[1];
-
-    reg = /year *= *\{(.*?)\}/;
-    s = details.match(reg);
-    let year = s[1];
-
-    reg = /organization *= *\{(.*?)\}/;
-    s = details.match(reg);
-    if (s == null) {
-        reg = /publisher *= *\{(.*?)\}/;
-        s = details.match(reg);
-    }
-    let organization = s[1];
-    return new Article(author, title, confjournal, volume, pages, year, organization);
 }
 
 function printElement(e, idelement) {
